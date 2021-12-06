d6729b14f1a0268a91b42c0c5b0cdeede4b68cb1
Sergii_Shcherbakov <Sergii_Shcherbakov@epam.com>
2021-12-06 23:17:26 +0200

    create first element.

--- react-is-fan/src/index.js
+++ react-is-fan/src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom'
+
+var style = {
+    backgroundColor: 'orange',
+    color: 'white',
+    fontfamily: 'Arial'
+}
+
+const title = React.createElement(
+    'h1',
+    {id: 'title', className: 'header', style: style},
+    'Hello World'
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    title,
+    document.getElementById('react-container')
+)
