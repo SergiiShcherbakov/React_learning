ed6ec35e69659aa5db939c55b63bc493e9a88c71
Sergii_Shcherbakov <Sergii_Shcherbakov@epam.com>
2021-12-07 23:33:14 +0200

    Create react component.

--- react-is-fan/src/index.js
+++ react-is-fan/src/index.js
@@ -7,11 +7,17 @@ var style = {
     fontfamily: 'Arial'
 }
 
+class Message extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello Everyone!</h1>
+            </div>
+        )
+    }
+}
 
 ReactDOM.render(
-    <div style={style}>
-        <h1 id="headding-element">Hello world!!!</h1>
-        <p>We're glad you're here!</p>
-    </div>,
+    <Message />,
     document.getElementById('react-container')
 )
