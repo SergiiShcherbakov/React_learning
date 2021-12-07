b7523ddc8793b8daed2eec338bb93a9a901efd0f
Sergii_Shcherbakov <Sergii_Shcherbakov@epam.com>
2021-12-07 23:39:35 +0200

    add properties to react class component.

--- react-is-fan/src/index.js
+++ react-is-fan/src/index.js
@@ -9,15 +9,16 @@ var style = {
 
 class Message extends React.Component {
     render() {
+        console.log(this.props)
         return (
             <div>
-                <h1>Hello Everyone!</h1>
+                <h1>Hello Everyone! {this.props.msg}</h1>
             </div>
         )
     }
 }
 
 ReactDOM.render(
-    <Message />,
+    <Message age={50} msg="How are you?"/>,
     document.getElementById('react-container')
 )
